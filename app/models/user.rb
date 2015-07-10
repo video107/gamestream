14b02ad22b717e9f689a83992fcd77f9ec0b93d8
John Chang <tschangc@gmail.com>
2015-07-10 17:04:01 +0800

    finished google with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # , :lockable, :timeoutable and, :confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, :omniauth_providers => [:facebook, :google]
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
 
   has_many :menu_users
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   has_many :follow_cases, :through => :case_followers, :source => :case
 
   # def self.from_omniauth(auth)
-  #   user = where(provider: auth.provider, fb_uid: auth.uid).first
+  #   user = where(provider: auth.provider, google_uid: auth.uid).first
   #   unless user
   #     user = self.new
   #     user.fb_uid = auth.uid
@@ -27,18 +27,24 @@ class User < ActiveRecord::Base
   #   end
   #   user
   # end
+
   def self.from_omniauth(auth)
-  where(provider: auth.provider, fb_uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    # user.name = auth.info.name   # assuming the user model has a name
-    user.fb_image = auth.info.image # assuming the user model has an image
+    where(provider: auth.provider, email: auth.info.email).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.fb_image = auth.info.image # assuming the user model has an image
+      user.google_uid = auth.uid
     end
   end
 
+
+
+
+
   def self.new_with_session(params, session)
       super.tap do |user|
-        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        if data = session["devise.google_data"] && session["devise.google_data"]["extra"]["raw_info"]
           user.email = data["email"] if user.email.blank?
         end
       end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'menus#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   resources :menus do
     resources :cases
   end
