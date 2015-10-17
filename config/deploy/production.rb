3afb4d41eca19008b7f9f7a75a791085fd35bef9
John Chang <tschangc@gmail.com>
2015-10-17 16:56:01 +0800

    Set staging deploy
    1.modify config/deploy.rb --> move set:deploy_to, '/home/deploy/gamestream'
       to  config/deploy/production.rb and config/deploy/staging.rb
    2.new add config/environment/staging.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@ server '139.162.3.89', user: 'deploy', roles: %w{app db web}, my_property: :my_v
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-
+set :deploy_to, '/home/deploy/gamestream'
 
 
 # role-based syntax
