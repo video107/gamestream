d735f3150e2b81e3128f555639e0f66f9b116624
John Chang <tschangc@gmail.com>
2015-12-31 22:01:57 +0800

    try to fix mailer server issue

--- app/models/withdraw_record.rb
+++ app/models/withdraw_record.rb
@@ -1,7 +1,7 @@
 class WithdrawRecord < ActiveRecord::Base
   belongs_to :user
 
-  validates_presence_of :withdrawaler_name, :withdrawaler_phone, :withdrawaler_bank_name, :withdrawaler_bank_branch_name, :withdrawaler_bank_name_code, :withdrawaler_bank_account
+  validates_presence_of :withdrawaler_name, :withdrawaler_phone, :withdrawaler_bank_name, :withdrawaler_bank_branch_name, :withdrawaler_bank_name_code, :withdrawaler_bank_account, :amount
   validates_length_of :withdrawaler_phone, :is => 10
   validates_numericality_of :amount, :greater_than_or_equal_to => 1000
   validate :amount_limitation
