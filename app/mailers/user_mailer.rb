48e42a615fc5664cc59aea90bc1348197908e087
John Chang <tschangc@gmail.com>
2016-03-17 22:36:02 +0800

    change gmail server address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,13 +4,13 @@ class UserMailer < ApplicationMailer
     @withdraw_record = withdraw_record
     @user = withdraw_record.user
 
-    mail( :to => "streamcircle2015@gmail.com", :subject => "#{@user.name}領款通知")
+    mail( :to => "streamcirclenet@gmail.com", :subject => "#{@user.name}領款通知")
   end
 
   def notify_feedback(feedback)
     @user = User.find_by_email(feedback.email)
     @feedback = feedback
 
-    mail( :to => "streamcircle2015@gmail.com", :subject => "#{@feedback.content[0..5]}...")
+    mail( :to => "streamcirclenet @gmail.com", :subject => "#{@feedback.content[0..5]}...")
   end
 end
